c7b0c55cf9a38158d29c99ff244f719d1d6fe8fe
andy-wittig <Awittig@nevada.unr.edu>
2024-06-30 01:32:21 -0700

    fixed hurt player area

--- scripts/hurt_player_area.gd
+++ scripts/hurt_player_area.gd
@@ -1,15 +1,29 @@
 extends Area2D
 
 @export var damage: int
-
+@export var attack_wait := 2.0
 var active := true
 
-func _on_body_entered(body):
-	if (active):
-		if (body.is_in_group("players") && multiplayer.is_server()):
+@onready var hurt_player_timer = $HurtPlayerTimer
+
+func apply_damage(body):
+	if (body.is_in_group("players") && multiplayer.is_server() && active):
 			if (body.get_name() == "player"): #single player
 				body.hurt_player(damage, global_position.x)
 			else: #multiplayer
 				body.hurt_player.rpc_id(body.player_id, damage, global_position.x)
 
-		
+func _on_body_entered(body):
+		hurt_player_timer.wait_time = attack_wait
+		hurt_player_timer.start()
+		apply_damage(body)
+
+func _on_body_exited(body):
+	if (get_overlapping_bodies().size() < 1): #no players left
+		hurt_player_timer.stop()
+
+func _on_hurt_player_timer_timeout():
+	for body in get_overlapping_bodies():
+		apply_damage(body)
+
+
