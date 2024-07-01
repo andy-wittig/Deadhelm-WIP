e5bccec746a26b0b5d94472326d3f79da23ff737
andy-wittig <157990692+andy-wittig@users.noreply.github.com>
2024-07-01 00:32:10 -0700

    small tweaks

--- scripts/blasphemer.gd
+++ scripts/blasphemer.gd
@@ -2,15 +2,15 @@ extends Node2D
 
 @onready var hurt_player_area = $HurtPlayerArea
 @onready var fire_particles = $FireParticles
-@onready var animation_player = $FlameLight/AnimationPlayer
+@onready var animation_player = $AnimationPlayer
 
 func _on_attack_timer_timeout():
 	hurt_player_area.active = not hurt_player_area.active
-
-func _process(_delta):
+	
 	if (hurt_player_area.active):
 		animation_player.play("flame")
 		fire_particles.emitting = true
 	else:
 		animation_player.play("RESET")
 		fire_particles.emitting = false
+	
