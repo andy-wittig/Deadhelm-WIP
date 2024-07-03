546f72afe68e6a16132241c6555be15cf28303ca
andy-wittig <Awittig@nevada.unr.edu>
2024-07-03 12:34:05 -0700

    coin tweaks

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -6,7 +6,9 @@ var rand_x_force = randf_range(-100, 100)
 var player = null
 var can_interact = false
 
-@rpc("any_peer", "call_local")
+@onready var detect_player = $DetectPlayer
+
+@rpc("any_peer", "call_local", "reliable")
 func destroy_self():
 	queue_free()
 	
@@ -14,30 +16,23 @@ func _on_timer_timeout():
 	queue_free()
 
 func _ready():
-	apply_central_impulse(Vector2(rand_x_force, y_force))
+	if (multiplayer.is_server()):
+		apply_central_impulse(Vector2(rand_x_force, y_force))
 
 func _process(delta):
-	if (can_interact):
-		if Input.is_action_just_pressed("pickup"):
-			player.collect_coin()
-			can_interact = false
-			rpc("destroy_self")
+	$AnimatedSprite2D.material.set_shader_parameter("enabled", false)
+	for body in detect_player.get_overlapping_bodies():
+		if (body.get_name() == "player"
+		|| body.player_id == multiplayer.get_unique_id()):
+			$AnimatedSprite2D.material.set_shader_parameter("enabled", true)
+			if Input.is_action_just_pressed("pickup"):
+				body.collect_coin()
+				rpc("destroy_self")
 
 func _on_input_event(viewport, event, shape_idx):
-	if (Input.is_action_just_pressed("left_click")):
-		if (can_interact):
-			player.collect_coin()
-			can_interact = false
-			rpc("destroy_self")
-
-func _on_detect_player_body_entered(body):
-	if (body.is_in_group("players")):
-			$AnimatedSprite2D.material.set_shader_parameter("enabled", true)
-			player = body
-			can_interact = true
-
-func _on_detect_player_body_exited(body):
-	if (body == player):
-		$AnimatedSprite2D.material.set_shader_parameter("enabled", false)
-		player = null
-		can_interact = false
+	for body in detect_player.get_overlapping_bodies():
+		if (body.get_name() == "player"
+		|| body.player_id == multiplayer.get_unique_id()):
+			if (Input.is_action_just_pressed("left_click")):
+				body.collect_coin()
+				rpc("destroy_self")
