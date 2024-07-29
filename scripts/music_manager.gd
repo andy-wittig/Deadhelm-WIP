fb124db6ddc6bd4458cd2ce58e6e8a02485d030f
andy-wittig <Awittig@nevada.unr.edu>
2024-07-29 16:23:19 -0700

    change animations

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -1,20 +1,26 @@
 extends Node
 
+var random = RandomNumberGenerator.new()
+
 @onready var title_track = $TitleTrack
 @onready var level_track_1 = $LevelTrack1
 @onready var level_track_2 = $LevelTrack2
+@onready var level_track_3 = $LevelTrack3
 
 func _process(delta):
 	if (GameManager.started_game):
 		for current_level in get_tree().get_root().get_node("game/Level").get_children():
 			if (current_level.get_name() == "level_1"):
-				if (not level_track_1.playing):
+				if (not level_track_1.playing && not level_track_2.playing):
 					stop_music()
-					level_track_1.play()
+					
+					var choice = random.randi_range(1, 2)
+					if (choice == 1): level_track_1.play()
+					else: level_track_2.play()	
 			elif (current_level.get_name() == "level_2"):
-				if (not level_track_2.playing):
+				if (not level_track_3.playing):
 					stop_music()
-					level_track_2.play()
+					level_track_3.play()
 	elif (not title_track.playing):
 		stop_music()
 		title_track.play()
