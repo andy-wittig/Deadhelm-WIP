55f62ce0b0aa9636ec9e9523d7415446c2a24c98
andy-wittig <Awittig@nevada.unr.edu>
2024-08-11 22:36:37 -0700

    bomb frog added

--- scripts/bomb_frog.gd
+++ scripts/bomb_frog.gd
@@ -1,20 +1,27 @@
 extends CharacterBody2D
 #Constants
-const SPEED = 24.0
-const JUMP_VELOCITY = -180.0
-const KNOCK_BACK_FORCE := 75.0
+const SPEED = 65.0
+const JUMP_VELOCITY = -200.0
+const HANG_TIME_THRESHHOLD := 40.0
+const HANG_TIME_MULTIPLIER := 0.5
+const KNOCK_BACK_SPEED := 75.0
+const DETONATE_TIME := 5.0
 const ROAM_CHANGE_WAIT := 6
-const ATTACK_RADIUS := 20
-const ATTACK_WAIT := 1
+const JUMP_WAIT := 2
+const ATTACK_RADIUS := 16
 #Movement Variables
 var rand_state_timer = RandomNumberGenerator.new()
 var direction: int
+var jump_timer: float
 var knock_back: Vector2
 #Attacking Variables
 var player = null
 var chasing_player = false
+var attack_timer_started = false
+var attacking = false
+var frog_detonated = false
 #Enemy Mechanics Variables
-var enemy_health = 50
+var enemy_health = 40
 var marked_for_death = false
 
 enum state_type {
@@ -27,34 +34,30 @@ var state := state_type.MOVING
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var chase_player = $ChasePlayer
+@onready var chase_player = $"chase player"
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
-@onready var animated_sprite = $AnimatedFesterSprite
+@onready var animated_sprite = $AnimatedSprite2D
 @onready var animation_player = $AnimationPlayer
-@onready var audio_player = $FesterScreamAudio
-@onready var hurt_player_area = $HurtPlayerArea
 	
 func _ready():
-	hurt_player_area.active = false
-	hurt_player_area.attack_wait = ATTACK_WAIT
-	
 	if (multiplayer.is_server() || !GameManager.multiplayer_mode_enabled):
 		direction = [-1, 1].pick_random()
 		%RoamTimer.start(randi_range(2, ROAM_CHANGE_WAIT))
 
 func _process(_delta):
-	if (multiplayer.is_server() || !GameManager.multiplayer_mode_enabled):
-		for body in chase_player.get_overlapping_bodies():
-			if (body.is_in_group("players")):
-				if (!chasing_player):
-					player = body
-					chasing_player = true
-					state = state_type.CHASE
-				return
-	#player left detection radius
-	player = null
-	chasing_player = false
+	if (not frog_detonated):
+		if (multiplayer.is_server() || !GameManager.multiplayer_mode_enabled):
+			for body in chase_player.get_overlapping_bodies():
+				if (body.is_in_group("players")):
+					if (!chasing_player):
+						player = body
+						chasing_player = true
+						state = state_type.CHASE
+					return
+		#player left detection radius
+		player = null
+		chasing_player = false
 
 func _physics_process(delta):
 	#deal with enemy death
@@ -79,62 +82,73 @@ func _physics_process(delta):
 				velocity.x = 0
 		state_type.MOVING:
 			if (multiplayer.is_server() || !GameManager.multiplayer_mode_enabled):
-				animated_sprite.play("run")
+				if (jump_timer <= 0):
+					if (is_on_floor()):
+						velocity.y = JUMP_VELOCITY
+					jump_timer = JUMP_WAIT
+				else:
+					jump_timer -= delta
 				
 				if ray_cast_right.is_colliding():
 					direction = -1
 				elif ray_cast_left.is_colliding():
 					direction = 1
 					
-				velocity.x = direction * SPEED
+				if (!is_on_floor()):
+					animated_sprite.play("leap")
+					velocity.x = direction * SPEED
+				else:
+					animated_sprite.play("idle")
+					velocity.x = move_toward(velocity.x, 0, SPEED)
 		state_type.CHASE:
 			if ((multiplayer.is_server() || !GameManager.multiplayer_mode_enabled) && player != null):
 				if (player.global_position.x - global_position.x > 8):
-						direction = 1
+					direction = 1
 				elif (player.global_position.x - global_position.x < -8):
 					direction = -1
-				
-				if (player.global_position.distance_to(global_position) > ATTACK_RADIUS):
-					hurt_player_area.active = false
-					animated_sprite.play("run")
+						
+				if (player.global_position.distance_to(global_position) > ATTACK_RADIUS):	
+					if (jump_timer <= 0):
+						if (is_on_floor()):
+							velocity.y = JUMP_VELOCITY
+						jump_timer = JUMP_WAIT
+					else:
+						jump_timer -= delta
 					
 					if (ray_cast_right.is_colliding() || ray_cast_left.is_colliding()
 					&& is_on_floor()):
 						velocity.y = JUMP_VELOCITY
 						
-					velocity.x = direction * SPEED
+					if (!is_on_floor()):
+						animated_sprite.play("leap")
+						velocity.x = direction * SPEED
+					else:
+						animated_sprite.play("idle")
+						velocity.x = move_toward(velocity.x, 0, SPEED)
 				else:
-					%CooldownTimer.start(ATTACK_WAIT)
-					animated_sprite.stop()
 					state = state_type.ATTACK
 		state_type.ATTACK:
-			audio_player.play()
-			
-			if (!animated_sprite.is_playing()):
-				animated_sprite.play("attack")
-				hurt_player_area.active = true
-				
 			velocity.x = 0
-
-	if not is_on_floor():
-		velocity.y += gravity * delta
+			animated_sprite.play("primed")
+			
+			if (animation_player.current_animation != "hurt_blink"):
+				animation_player.play("blink")
+			
+			if not attack_timer_started:
+				%AttackTimer.start(DETONATE_TIME)
+				attack_timer_started = true
+		
+	if (!is_on_floor()):
+		if (abs(velocity.y) < HANG_TIME_THRESHHOLD):
+			velocity.y += gravity * HANG_TIME_MULTIPLIER * delta
+		else:
+			velocity.y += gravity * delta
 	
 	velocity += knock_back
-	knock_back.x = move_toward(knock_back.x, 0, KNOCK_BACK_FORCE)
-	knock_back.y = move_toward(knock_back.y, 0, KNOCK_BACK_FORCE)
+	knock_back.x = move_toward(knock_back.x, 0, KNOCK_BACK_SPEED)
+	knock_back.y = move_toward(knock_back.y, 0, KNOCK_BACK_SPEED)
 		
 	move_and_slide()
-	
-func _on_animated_fester_sprite_animation_finished():
-	if (state == state_type.ATTACK):
-		animated_sprite.play("idle")
-		hurt_player_area.active = false
-	
-func _on_cooldown_timer_timeout():
-	if (chasing_player):
-		state = state_type.CHASE
-	else:
-		state = state_type.MOVING
 	
 func _on_change_state_timer_timeout():
 	if (not chasing_player):
@@ -142,6 +156,26 @@ func _on_change_state_timer_timeout():
 			direction = [-1, 1].pick_random()
 			state = randi_range(0, 1)
 			%RoamTimer.start(randf_range(0, ROAM_CHANGE_WAIT))
+
+func _on_cooldown_timer_timeout():
+	player = null
+	chasing_player = false
+	frog_detonated = false
+
+func _on_attack_timer_timeout():
+	attack_timer_started = false
+	if (chasing_player):
+		if (multiplayer.is_server()):
+			rpc("detonate")
+		elif (!GameManager.multiplayer_mode_enabled):
+			detonate()
+		frog_detonated = true
+		%CooldownTimer.start(4)
+		state = state_type.MOVING
+
+@rpc("call_local")
+func detonate():
+	print("boom!")
 
 func apply_knockback(other_pos: Vector2, force: float):
 	var other_dir = (other_pos - global_position).normalized()
@@ -164,9 +198,9 @@ func destroy_self():
 	var soul = load("res://scenes/level_objects/soul.tscn").instantiate()
 	var death_effect = load("res://scenes/vfx/bombshell_turtle_death.tscn").instantiate()
 	soul.position = position
-	soul.rarities["diamond"] = 60
-	soul.rarities["emerald"] = 25
-	soul.rarities["gold"] = 5
+	soul.rarities["diamond"] = 70
+	soul.rarities["emerald"] = 18
+	soul.rarities["gold"] = 2
 	soul.rarities["ruby"] = 10
 	death_effect.position = position
 	get_parent().add_child(soul)
