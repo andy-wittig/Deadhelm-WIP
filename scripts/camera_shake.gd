fc4b0377931145a3da3d1f291f4315c9c3b56fee
andy-wittig <Awittig@nevada.unr.edu>
2024-09-08 21:01:53 -0700

    camera shake fixed

--- scripts/camera_shake.gd
+++ scripts/camera_shake.gd
@@ -2,25 +2,27 @@ extends Camera2D
 
 @export var noise : FastNoiseLite
 
-const LERP_SPEED := 0.8
-const OFFSET_DAMP := 0.5
+const LERP_SPEED := 0.9
+const OFFSET_DAMP := 0.4
 
 var decay := 0.9
-var max_offset := Vector2(16, 12)
+var max_offset := Vector2(16, 16)
 var max_roll := 0.1
 var trauma := 0.0
-var trauma_power := 3
 var noise_y := 0
 	
 func add_trauma(amount):
 	trauma = min(trauma + amount, 1.0)
 	
 func shake():
-	var amount = pow(trauma, trauma_power)
+	var amount = trauma
 	noise_y += 1
 	rotation = max_roll * amount * noise.get_noise_2d(0, noise_y)
-	offset.x = max_offset.x * amount * noise.get_noise_2d(1000, noise_y)
-	offset.y = max_offset.y * amount * noise.get_noise_2d(2000, noise_y)
+	var new_offset : Vector2
+	new_offset.x = max_offset.x * amount * noise.get_noise_2d(1000, noise_y)
+	new_offset.y = max_offset.y * amount * noise.get_noise_2d(2000, noise_y)
+	
+	position = new_offset
 	
 func _process(delta):
 	if (trauma):
