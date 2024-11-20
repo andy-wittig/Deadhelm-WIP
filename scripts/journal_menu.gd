0ff6c87a2eb04b592498d3493b01b1d53e50096a
andy-wittig <Awittig@nevada.unr.edu>
2024-11-20 11:41:17 -0800

    journal chapters added

--- scripts/journal_menu.gd
+++ scripts/journal_menu.gd
@@ -2,28 +2,33 @@ extends Control
 
 @onready var menu_layer = $"../.."
 
-var page_dict = {
-	0 : ["res://assets/journal_pages/journal_entry_blasphemer.txt",
-	"res://assets/journal_pages/journal_description_blasphemer.txt", true],
-	1 : ["res://assets/journal_pages/journal_entry_desecrator.txt",
-	"res://assets/journal_pages/journal_description_desecrator.txt", true],
-	2 : ["res://assets/journal_pages/journal_entry_bombshell.txt",
-	"res://assets/journal_pages/journal_description_bombshell.txt", true],
-	3 : ["res://assets/journal_pages/journal_entry_bomb_frog.txt",
-	"res://assets/journal_pages/journal_description_bomb_frog.txt", true],
-	4 : ["res://assets/journal_pages/journal_entry_sentinel.txt",
-	"res://assets/journal_pages/journal_description_sentinel.txt", true],
-	5 : ["res://assets/journal_pages/journal_entry_fester.txt",
-	"res://assets/journal_pages/journal_description_fester.txt", true],
-	6 : ["res://assets/journal_pages/journal_entry_irritus.txt",
-	"res://assets/journal_pages/journal_description_irritus.txt", true],
-	7 : ["res://assets/journal_pages/journal_entry_golem.txt",
-	"res://assets/journal_pages/journal_description_golem.txt", true],
-	8 : ["res://assets/journal_pages/journal_entry_scorpion.txt",
-	"res://assets/journal_pages/journal_description_scorpion.txt", true],
-	9 : ["res://assets/journal_pages/journal_entry_snake.txt",
-	"res://assets/journal_pages/journal_description_snake.txt", true],
-}
+var spell_pages = [
+	["res://assets/journal_pages/journal_lightning_spell.txt", true],
+	["res://assets/journal_pages/journal_shield_spell.txt", true],
+]
+
+var enemy_pages = [
+	["res://assets/journal_pages/journal_entry_blasphemer.txt", true],
+	["res://assets/journal_pages/journal_description_blasphemer.txt", true],
+	["res://assets/journal_pages/journal_entry_desecrator.txt", true],
+	["res://assets/journal_pages/journal_description_desecrator.txt", true],
+	["res://assets/journal_pages/journal_entry_bombshell.txt", true],
+	["res://assets/journal_pages/journal_description_bombshell.txt", true],
+	["res://assets/journal_pages/journal_entry_bomb_frog.txt", true],
+	["res://assets/journal_pages/journal_description_bomb_frog.txt", true],
+	["res://assets/journal_pages/journal_entry_sentinel.txt", true],
+	["res://assets/journal_pages/journal_description_sentinel.txt", true],
+	["res://assets/journal_pages/journal_entry_fester.txt", true],
+	["res://assets/journal_pages/journal_description_fester.txt", true],
+	["res://assets/journal_pages/journal_entry_irritus.txt", true],
+	["res://assets/journal_pages/journal_description_irritus.txt", true],
+	["res://assets/journal_pages/journal_entry_golem.txt", true],
+	["res://assets/journal_pages/journal_description_golem.txt", true],
+	["res://assets/journal_pages/journal_entry_scorpion.txt", true],
+	["res://assets/journal_pages/journal_description_scorpion.txt", true],
+	["res://assets/journal_pages/journal_entry_snake.txt", true],
+	["res://assets/journal_pages/journal_description_snake.txt", true],
+]
 
 var locked_page := "
 [center]
@@ -32,51 +37,69 @@ var locked_page := "
 [img={width%}128{height%}]res://assets/sprites/UI/player_information/placeholder_lock.png[/img]
 
 Discover and defeat new enemies to unlock this page.
-[/center]
-"
-var journal_length : int = page_dict.size()
+[/center]"
+
+var chapter = {
+	"Spell" : spell_pages,
+	"Enemy" : enemy_pages,
+}
+var current_chapter := "Spell"
+var journal_length : int = chapter[current_chapter].size()
 var journal_page := 0
 var menu_started := false
 
+@onready var left_page = $VBoxContainer/Pages/LeftPage
+@onready var right_page = $VBoxContainer/Pages/RighPage
+@onready var tab_bar = $VBoxContainer/TabBar
+
 func _ready():
-	load_pages(page_dict[0][0], page_dict[0][1])
-	
+	load_pages(chapter[current_chapter][0][0], chapter[current_chapter][1][0])
+			
 func load_pages(page_1: String, page_2: String):
-	if (page_dict[journal_page][2]): #pages are unlocked
+	if (chapter[current_chapter][journal_page][1]): #left page unlocked
 		var read_page_1 = FileAccess.open(page_1, FileAccess.READ)
-		var read_page_2 = FileAccess.open(page_2, FileAccess.READ)
 		var page_content_1 := read_page_1.get_as_text()
+		left_page.text = page_content_1
+	else:
+		left_page.text = locked_page
+	
+	if (chapter[current_chapter][journal_page + 1][1]): #right page unlocked
+		var read_page_2 = FileAccess.open(page_2, FileAccess.READ)
 		var page_content_2 := read_page_2.get_as_text()
 		
-		$VBoxContainer/Pages/JournalEntry.text = page_content_1
-		$VBoxContainer/Pages/DescriptionEntry.text = page_content_2
+		right_page.text = page_content_2
 	else:
-		$VBoxContainer/Pages/JournalEntry.text = locked_page
-		$VBoxContainer/Pages/DescriptionEntry.text = locked_page
+		right_page.text = locked_page
 
-func unlock_page(page_number : int):
-	page_dict[page_number][2] = true
+func unlock_page(chapter_type : String, page_number : int):
+	chapter[chapter_type][page_number][2] = true
 
 func menu_opened():
 	if (!menu_started):
-		#$BackButton.grab_focus()
+		tab_bar.grab_focus()
 		menu_started = true
-		load_pages(page_dict[0][0], page_dict[0][1])
+		load_pages(chapter[current_chapter][0][0], chapter[current_chapter][1][0])
 
 func _on_back_button_pressed():
 	menu_started = false
 	menu_layer.return_to_prev_menu()
 
 func _on_prev_page_button_pressed():
-	journal_page -= 1
-	journal_page = clamp(journal_page, 0, journal_length - 1)
-	$VBoxContainer/PageNumber/LeftPageNumber.text = str((journal_page * 2) + 1)
-	$VBoxContainer/PageNumber/RightPageNumber.text = str((journal_page * 2) + 2)
-	load_pages(page_dict[journal_page][0], page_dict[journal_page][1])
+	journal_page -= 2
+	journal_page = clamp(journal_page, 0, journal_length - 2)
+	$VBoxContainer/PageNumber/LeftPageNumber.text = str((journal_page) + 1)
+	$VBoxContainer/PageNumber/RightPageNumber.text = str((journal_page) + 2)
+	load_pages(chapter[current_chapter][journal_page][0], chapter[current_chapter][journal_page + 1][0])
 
 func _on_next_page_button_pressed():
-	journal_page += 1
-	journal_page = clamp(journal_page, 0, journal_length - 1) 
-	$VBoxContainer/PageNumber/LeftPageNumber.text = str((journal_page * 2) + 1)
-	$VBoxContainer/PageNumber/RightPageNumber.text = str((journal_page * 2) + 2)
-	load_pages(page_dict[journal_page][0], page_dict[journal_page][1])
+	journal_page += 2
+	journal_page = clamp(journal_page, 0, journal_length - 2) 
+	$VBoxContainer/PageNumber/LeftPageNumber.text = str((journal_page) + 1)
+	$VBoxContainer/PageNumber/RightPageNumber.text = str((journal_page) + 2)
+	load_pages(chapter[current_chapter][journal_page][0], chapter[current_chapter][journal_page + 1][0])
+
+func _on_tab_bar_tab_changed(tab):
+	current_chapter = chapter.keys()[tab_bar.current_tab]
+	journal_page = 0
+	journal_length = chapter[current_chapter].size()
+	load_pages(chapter[current_chapter][0][0], chapter[current_chapter][1][0])
